8b3afba399a26840e83600da082e2402a805439f
Arslan Ahmad <mr.arsal69@gmail.com>
2026-04-17 00:29:30 +0500

    updated config

--- app.py
+++ app.py
@@ -749,6 +749,7 @@ def create_chrome_driver():
         pass  # Ignore errors
     
     # Additional options for stability and compatibility
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
