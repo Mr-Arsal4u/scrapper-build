8a213d9762001706f75cf54fca12273dc48da481
Arslan Ahmad <mr.arsal69@gmail.com>
2026-04-17 01:33:34 +0500

    format

--- app.py
+++ app.py
@@ -737,6 +737,8 @@ def create_chrome_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=0")
+    chrome_options.add_argument("--single-process")
     chrome_options.add_argument("--disable-software-rasterizer")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
